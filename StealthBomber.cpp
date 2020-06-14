70818c06fa18b41dfa55fbd6ed7d6c7653a9c68d
alexavila0128@gmail.com <alexavila0128@gmail.com>
2020-06-14 11:43:48 +0200

    Turret fixed

--- StealthBomber.cpp
+++ StealthBomber.cpp
@@ -109,9 +109,5 @@ void StealthBomber::Update()
 	// Call to the base class. It must be called at the end
 	// It will update the collider depending on the position
 	Enemy::Update();
-
-	
-	
-	
 }
 
